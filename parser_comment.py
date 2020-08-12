bf2cd5fa6d03bdaad02e2723cac8ecd25683bfbc
fox0 <red-fox0@yandex.ru>
2020-08-12 20:19:04 +0700

    up

--- parser_comment.py
+++ parser_comment.py
@@ -13,7 +13,7 @@ def main(author_id, pages):
     for p in range(pages, 0, -1):
         r = s.get(url, params=dict(p=p))
         soup = BeautifulSoup(r.text, 'lxml')
-        for i in reversed(soup.find_all('article', {'class': 'comment_thumb'})):
+        for i in reversed(soup.find_all('article', {'class': 'comment-container'})):
             link = i.find('div', {'class': 'comment_link_to_fic'})
             print('https://ficbook.net' + link.a['href'])
             print('Фанфик: ' + link.text.strip())
@@ -28,4 +28,4 @@ def main(author_id, pages):
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s [%(levelname)s] %(name)s %(funcName)s():%(lineno)d: %(message)s',
                         level=logging.DEBUG)
-    main(author_id=1, pages=59)
+    main(author_id=1880805, pages=150)
