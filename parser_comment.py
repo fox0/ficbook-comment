dee8e9110adf857be2a3d7f989a03f77d57b43e8
fox0 <red-fox0@yandex.ru>
2022-08-26 01:14:15 +0700

    Update parser_comment.py

--- parser_comment.py
+++ parser_comment.py
@@ -10,10 +10,15 @@ log = logging.getLogger(__name__)
 def main(author_id, pages):
     url = url_template % author_id
     s = requests.Session()
+    s.headers.update({
+        # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36',
+    })
     for p in range(pages, 0, -1):
         r = s.get(url, params=dict(p=p))
+        assert r.status_code == 200
         soup = BeautifulSoup(r.text, 'lxml')
-        for i in reversed(soup.find_all('article', {'class': 'comment-container'})):
+        for i in reversed(soup.find_all('div', {'class': 'comment-content'})):
             link = i.find('div', {'class': 'comment_link_to_fic'})
             print('https://ficbook.net' + link.a['href'])
             print('Фанфик: ' + link.text.strip())
